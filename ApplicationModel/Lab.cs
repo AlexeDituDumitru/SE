898ac7fd1379ba5772c2feec65c9365a80f82778
AlexeDituDumitru <38571661+AlexeDituDumitru@users.noreply.github.com>
2018-05-30 02:46:45 +0300

    Add files via upload

---
+++ ApplicationModel/Lab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationModel
+{
+    class Lab
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ConditionToPass { get; set; }
+        public int NumberOfWeeks { get; set; }
+        public virtual Teacher Teacher { get; set; }
+    }
+}
